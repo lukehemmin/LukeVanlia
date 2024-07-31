b7dd946afb1b789ad8906845b3e5f8a6abfa0767
박상호 Sangho Park <ps040211@gmail.com>
2024-07-31 16:32:29 +0900

    콘솔 공지 명령어 추가 ?, help, pl, plugin 명령어 차단

--- src/main/java/com/lukehemmin/lukeVanlia/Main.kt
+++ src/main/java/com/lukehemmin/lukeVanlia/Main.kt
@@ -1,18 +1,17 @@
 package com.lukehemmin.lukeVanlia
 
 import com.lukehemmin.lukeVanlia.commands.admincommandsprotect
-import com.lukehemmin.lukeVanlia.event.joinquitbrodcast
-import com.lukehemmin.lukeVanlia.event.serverjoin
-import com.lukehemmin.lukeVanlia.item.itemevent
-import com.lukehemmin.lukeVanlia.item.itemgive
 import com.lukehemmin.lukeVanlia.lobby.lobbysystem
 import com.lukehemmin.lukeVanlia.lobby.SnowMinigame
+import com.lukehemmin.lukeVanlia.velocity.castmessage
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     private lateinit var snowMinigame: SnowMinigame
     override fun onEnable() {
         logger.info("Plugin enabled")
+
+        getCommand("castmessage")?.setExecutor(castmessage())
 
         // Custom Item Command / Event
         //getCommand("ticket_give")?.setExecutor(itemgive())
@@ -25,12 +24,15 @@ class Main : JavaPlugin() {
         //server.pluginManager.registerEvents(joinquitbrodcast(this), this)
 
         // admincommandsprotect commands
-        getCommand("pl")?.setExecutor(admincommandsprotect())
+        //getCommand("pl")?.setExecutor(admincommandsprotect())
 
         // Lobby System
         val lobbySystem = lobbysystem(this)
         server.pluginManager.registerEvents(lobbySystem, this)
         getCommand("help")?.setExecutor(lobbySystem)
+        getCommand("?")?.setExecutor(lobbySystem)
+        getCommand("pl")?.setExecutor(lobbySystem)
+        getCommand("plugin")?.setExecutor(lobbySystem)
 
         snowMinigame = SnowMinigame(this)
         server.pluginManager.registerEvents(snowMinigame, this)
